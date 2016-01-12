22c852d90178f9085cd9de4e07c43edd061080b1
lcricco <lcricco@users.noreply.github.com>
2016-01-11 17:35:56 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,13 @@
 file <- "household_power_consumption.txt"
-data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+epower <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subset_epower <- epower[epower$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+datetime <- strptime(paste(subset_epower$Date, subset_epower$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subset_epower$Global_active_power)
+subMetering1 <- as.numeric(subset_epower$Sub_metering_1)
+subMetering2 <- as.numeric(subset_epower$Sub_metering_2)
+subMetering3 <- as.numeric(subset_epower$Sub_metering_3)
 
 png("plot3.png", width=480, height=480)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
